44791e889ab0ce8249ab865e1fc4611d0368ed17
Manomit Mitra <manomit@wrctechnologies.com>
2017-08-04 11:20:51 +0530

    commit by manomit

---
+++ database/migrations/2017_08_03_112105_create_vendors_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->text('address');
+            $table->integer('country_id')->unsigned();
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->string('city');
+            $table->string('pincode',10);
+            $table->string('phone', 50);
+            $table->string('fax', 50);
+            $table->string('toll_free');
+            $table->string('contact_person');
+            $table->string('contact_person_role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
